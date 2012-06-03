e37906b6b3bb079f99b9e25f6ba1ffaff8ff3432
bonfa <francesco.bonfadelli@gmail.com>
2012-06-03 14:52:35 +0200

    terminato il refactoring

--- src/test/test_all.py
+++ src/test/test_all.py
@@ -8,6 +8,6 @@ import unittest
 
 
 if __name__ == '__main__':
-	start_dir = './'
+	start_dir = '/media/DATA/06-WorkSpace/netsec_wp/src/test/'
 	suite = unittest.TestLoader().discover(start_dir)	
 	unittest.TextTestRunner(verbosity=2).run(suite)
