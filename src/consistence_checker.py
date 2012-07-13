99ed287ff8eee11099175dadf433635055f68ee9
francesco <francesco.bonfadelli@gmail.com>
2012-07-13 10:46:37 +0200

    inserita la struttura dei metodi di check

--- src/consistence_checker.py
+++ src/consistence_checker.py
@@ -20,10 +20,40 @@ class FourWayHandshakeConsistenceChecker():
 
 	def isConsistent(self):
 		'''
-		Ritorna true se 
+		Ritorna true se tutti i test di consistenza hanno successo
 		'''
-		p1_consistence = self.firstPacketCosistent
-		p2_consistence = self.secondPacketCosistent
-		p3_consistence = self.thirdPacketCosistent	
-		p4_consistence = self.fourthPacketCosistent
-		return (p1_consistence and p2_consistence and p3_consistence and p4_consistence)
+		p1_consistence = self.firstPacketCosistent()
+		p2_consistence = self.secondPacketCosistent()
+		p3_consistence = self.thirdPacketCosistent()	
+		p4_consistence = self.fourthPacketCosistent()
+		packets_address_consistence = self.macAddressConsistence()
+		return (p1_consistence and p2_consistence and p3_consistence and p4_consistence and packets_address_consistence)
+
+	
+	def firstPacketCosistent(self):
+		'''
+		Ritorna true se i test di consistenza hanno successo sul primo pacchetto
+		'''
+	
+	def secondPacketCosistent(self):
+		'''
+		Ritorna true se i test di consistenza hanno successo sul secondo pacchetto
+		'''
+	
+	def thirdPacketCosistent(self):
+		'''
+		Ritorna true se i test di consistenza hanno successo sul terzo pacchetto
+		'''
+	
+	
+	def fourthPacketCosistent(self):
+		'''
+		Ritorna true se i test di consistenza hanno successo sul quarto pacchetto
+		'''
+	
+	
+	def macAddressConsistence(self):
+		'''
+		Ritorna true se i test di consistenza sui mac address hanno successo
+		'''
+	
