15104446b0e04a72b5f462fb2d382a8078aff001
francesco <francesco.bonfadelli@gmail.com>
2012-07-13 15:31:42 +0200

    inserito un po di codice

--- src/consistence_checker.py
+++ src/consistence_checker.py
@@ -56,4 +56,27 @@ class FourWayHandshakeConsistenceChecker():
 		'''
 		Ritorna true se i test di consistenza sui mac address hanno successo
 		'''
-	
+		#Controllo gli indirizzi del primo e del terzo pacchetto
+		if self.packet_1.src != self.packet_3.src
+			raise PacketError('self.packet_1.src != self.packet_3.src','Different source address')
+				
+		if self.packet_1.dst != self.packet_3.dst
+			raise PacketError('self.packet_1.dst != self.packet_3.dst','Different destination address')
+		
+		#Controllo gli indirizzi del secondo e del quarto pacchetto	
+		if self.packet_2.src != self.packet_4.src
+			raise PacketError('self.packet_2.src != self.packet_4.src','Different source address')
+		if self.packet_2.dst != self.packet_4.dst
+			raise PacketError('self.packet_2.dst != self.packet_4.dst','Different destination address')
+			
+		#Controllo che l'indirizzo sorgente del primo pacchetto corrisponda all'indirizzo destinazione del secondo 
+		if self.packet_1.src != self.packet_2.dst
+			raise PacketError('self.packet_1.src != self.packet_2.dst','Addresses must be equal')
+		if self.packet_2.src != self.packet_1.dst
+			raise PacketError('self.packet_2.src != self.packet_1.dst','Addresses must be equal')	
+		
+		return True
+		
+	@Classmethod	
+	def checkPacketConsistence(self,packet,original):
+			
