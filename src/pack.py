5108783be8a4b5e4bdfa186473101abc6ac7b09a
bonfa <francesco.bonfadelli@gmail.com>
2012-07-16 19:22:21 +0200

    inserita la struttura per i pacchetti

---
+++ src/pack.py
@@ -0,0 +1,16 @@
+#/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Modulo che contiene la struttura packet
+'''
+
+class Pacchetto():
+	
+	def __init__(self,pcapForm,objectForm,scapyForm):
+		self.pcapForm = pcapForm
+		self.objectForm = objectForm
+		self.scapyForm = scapyForm
+		
+
+	
