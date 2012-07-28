5976a9b822aa1f0f3cb9b59baa1a0cdda3c6ce03
bonfa <francesco.bonfadelli@gmail.com>
2012-07-28 22:13:20 +0200

    inserito una seconda serie di parametri

---
+++ src/wwwlan_july.py
@@ -0,0 +1,21 @@
+#/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import subprocess
+import sys
+
+path = '../pacchetti-catturati/wwwlan_02/'
+NomePacchetto1_4Way = path + 'four_way_1.pcap'
+NomePacchetto2_4Way = path + 'four_way_2.pcap'
+NomePacchetto3_4Way = path + 'four_way_3.pcap'
+NomePacchetto4_4Way = path + 'four_way_4.pcap'
+nomePacchettoDati = path + 'criptato.pcap'
+#nomePacchettoDati = path + 'fish0all.pcap'	
+
+pms = 'H6x&@!1uLQ*()!12c0x\\f^\'?|s<SNgh-'
+ssid = 'WWWLAN'
+
+
+# Eseguo PlateDetector sull'immagine
+wpaDecryptor = subprocess.Popen([sys.executable,"./main.py",pms,ssid,NomePacchetto1_4Way,NomePacchetto2_4Way,NomePacchetto3_4Way,NomePacchetto4_4Way,nomePacchettoDati],stdout=None, stderr=None) 
+decriptorStatus = wpaDecryptor.communicate()
